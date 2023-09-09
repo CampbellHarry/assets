9f440f1f63eeb1623472a0dad51341d99c869e30
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-09-09 15:32:13 +0100

    Add files via upload

--- assets/Info mod/main.js
+++ assets/Info mod/main.js
@@ -39,6 +39,7 @@ document.getElementById("addButton").addEventListener("click", function() {
 
         var linepoint = document.getElementById("linepoints" + warningCount);
         linepoint.style.transform = "scale(1.2)";
+        linepoint.style.transition = "1s ease-in";
 
         var linepointz = document.getElementById("linepointz");
         linepointz.classList.remove("hidden");
